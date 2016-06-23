5b31b932e494c27a6b691b8ad40736e669509d61
Michael Muinos <michaelmuinos@yahoo.com>
2016-06-22 22:15:53 -0700

    print statements will now popup through the text widget on the GUI, automatic scroll is functional

--- Converter.py
+++ Converter.py
@@ -39,9 +39,12 @@ class GUI:
         print("Starting download...", file=buffer)
         self.update_text_widget(buffer)
 
-        song_list = self.read_file(self.entry.get(), buffer)
+        song_list = self.read_file(self.entry.get())
         video_urls = self.find_video_urls(song_list, buffer)
         self.download_videos(video_urls, buffer)
+
+        print("All downloads complete!", file=buffer)
+        self.update_text_widget(buffer)
 
     def update_text_widget(self, buffer):
         output = buffer.getvalue()
@@ -53,9 +56,13 @@ class GUI:
             songs = f.read().splitlines()
         return songs
 
-    def find_video_urls(songs):
+    def find_video_urls(self, songs, buffer):
         videos = list()
+        index = 1
         for song in songs:
+            print("Song #" + str(index) + ": " + song, file=buffer)
+            self.update_text_widget(buffer)
+
             query_string = urllib.parse.urlencode({"search_query": song})
             html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
             search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
@@ -64,7 +71,7 @@ class GUI:
                 videos.append("https://www.youtube.com/watch?v=" + search_results[0])
         return videos
 
-    def download_videos(urls):
+    def download_videos(self, urls, buffer):
         for url in urls:
             with youtube_dl.YoutubeDL(youtube_dl_options) as ydl:
                 ydl.download([url])
