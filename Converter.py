728caa3645849f10a7785e835f0b6d93fbbf9fc9
Michael Muinos <michaelmuinos@yahoo.com>
2016-06-22 22:09:49 -0700

    integrated GUI components

--- Converter.py
+++ Converter.py
@@ -39,43 +39,36 @@ class GUI:
         print("Starting download...", file=buffer)
         self.update_text_widget(buffer)
 
-
+        song_list = self.read_file(self.entry.get(), buffer)
+        video_urls = self.find_video_urls(song_list, buffer)
+        self.download_videos(video_urls, buffer)
 
     def update_text_widget(self, buffer):
         output = buffer.getvalue()
         self.text.insert(END, output)
         self.text.see(END)
 
+    def read_file(self, file_name):
+        with open(file_name, "r") as f:
+            songs = f.read().splitlines()
+        return songs
 
-def read_file(file_name):
-    with open(file_name, "r") as f:
-        songs = f.read().splitlines()
+    def find_video_urls(songs):
+        videos = list()
+        for song in songs:
+            query_string = urllib.parse.urlencode({"search_query": song})
+            html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
+            search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
 
-    return songs
+            if len(search_results) is not 0:
+                videos.append("https://www.youtube.com/watch?v=" + search_results[0])
+        return videos
 
-
-def find_video_urls(songs):
-    videos = list()
-    for song in songs:
-        query_string = urllib.parse.urlencode({"search_query": song})
-        html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
-        search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
-
-        if len(search_results) is not 0:
-            videos.append("https://www.youtube.com/watch?v=" + search_results[0])
-
-    return videos
-
-
-def download_videos(urls):
-    for url in urls:
-        with youtube_dl.YoutubeDL(youtube_dl_options) as ydl:
-            ydl.download([url])
-    print("Downloads Complete")
-
-#song_list = read_file(r"C:\Users\Luke\PycharmProjects\YoutubeToMp3\Songs.txt")
-#video_urls = find_video_urls(song_list)
-#download_videos(video_urls)
+    def download_videos(urls):
+        for url in urls:
+            with youtube_dl.YoutubeDL(youtube_dl_options) as ydl:
+                ydl.download([url])
+        print("Downloads Complete")
 
 root = Tk()
 root.resizable(width=False, height=False)
