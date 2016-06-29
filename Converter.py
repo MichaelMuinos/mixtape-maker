ec8fc5bd53f3255ee8fb85d44dc6cc65466bcaff
michael <michaelmuinos@yahoo.com>
2016-06-29 10:04:05 -0700

    changed to use subprocess module for output directory, removed create desktop directory function

--- Converter.py
+++ Converter.py
@@ -6,7 +6,8 @@ import re
 import os
 import shutil
 import threading
-import io
+import getpass
+import subprocess
 
 youtube_dl_options = {
     'format': 'bestaudio/best',
@@ -18,7 +19,8 @@ youtube_dl_options = {
 }
 
 song_list = []
-desktop_path = "C:\\Users\\Luke\\Desktop\\"
+# desktop_path = "C:\\Users\\Luke\\Desktop\\"
+desktop_path = "/Users/" + getpass.getuser() + "/Desktop/"
 
 
 class GUI:
@@ -27,14 +29,26 @@ class GUI:
         master.title("Mix-tape Maker")
         master.geometry("750x500")
 
-        self.label = Label(master, text="Enter the directory to song list:")
+        self.label = Label(master, text="Enter file name for the song list: ")
         self.label.pack()
 
         self.entry = Entry(master, width=45)
         self.entry.pack()
 
+        self.label_title = Label(master, text="Title of Mixtape: ")
+        self.label_title.pack()
+
+        self.entry_title = Entry(master, width=45)
+        self.entry_title.pack()
+
         self.button = Button(master, text="Download", command=self.download_button_click)
         self.button.pack()
+
+        self.label_help = Label(master, text="\nHOW TO USE:\n1. Create a text file on your desktop listing each song you want line by line."
+                                             "\n2. Enter the text file name inside the top most box.\n3. Give a title to your mixtape.\n"
+                                             "4. Click download and the mixtape will be created on your desktop."
+                                             "\n-----------------------------------------------------------------------------------\n")
+        self.label_help.pack()
 
         self.text = Text(master)
         self.text.pack()
@@ -54,8 +68,8 @@ class GUI:
 
         with open(file, "r") as f:
             songs = f.read().splitlines()
-            title = songs.pop(0)
-        desktop_path += title + "\\"
+        # change to "\\" for windows
+        desktop_path += self.entry_title.get() + "/"
 
         self.update_text_widget("\nFound " + str(len(songs)) + " songs!")
 
@@ -74,7 +88,7 @@ class DownloadVideosThread(threading.Thread):
     def run(self):
         video_urls = self.find_video_urls(song_list)
         self.download_videos(video_urls, song_list)
-        self.create_desktop_directory(song_list)
+        # self.create_desktop_directory(song_list)
 
     def update_text_widget(self, message):
         self.text.insert(END, message)
@@ -101,32 +115,34 @@ class DownloadVideosThread(threading.Thread):
         for url in urls:
             self.update_text_widget("\nSong - " + songs[index]
                                     + " - Extracting audio from video at " + url + " ...")
-            with youtube_dl.YoutubeDL(youtube_dl_options) as ydl:
-                ydl.download([url])
+            '''with youtube_dl.YoutubeDL(youtube_dl_options) as ydl:
+                ydl.download([url])'''
+            subprocess.call(
+                ['youtube-dl', '-o', desktop_path + songs[index] + '.mp3',
+                 "--extract-audio", "--audio-format", "mp3", url])
 
             self.update_text_widget("\nSong - " + songs[index] + " - Download complete!")
             index += 1
 
-        self.update_text_widget("\n\nAll downloads complete!")
+        self.update_text_widget("\n\nAll downloads complete! Check your desktop for your mixtape!")
 
-    def create_desktop_directory(self, songs):
+    '''def create_desktop_directory(self, songs):
         self.update_text_widget("\n\nPushing mixtape to desktop ...")
 
         if not os.path.exists(desktop_path):
             os.makedirs(desktop_path)
 
-        program_path = "C:\\Users\\Luke\\PycharmProjects\\MixtapeMaker\\"
+        # program_path = "C:\\Users\\Luke\\PycharmProjects\\MixtapeMaker\\"
+        program_path = "/Users/michael/PycharmProjects/MixtapeMaker/"
         contents = os.listdir(program_path)
 
         song_number = 0
         for content_part in contents:
             filename, file_extension = os.path.splitext(program_path + content_part)
             if file_extension == ".mp3":
-                print("\\\\\\\\\\\\CONTENT: " + content_part)
-                print("\\\\\\\\\\\\SONG: " + songs[song_number])
                 shutil.move(program_path + content_part, desktop_path + songs[song_number] + ".mp3")
                 song_number += 1
-        self.update_text_widget("\nMixtape is complete, enjoy!")
+        self.update_text_widget("\nMixtape is complete, enjoy!")'''
 
 root = Tk()
 root.resizable(width=False, height=False)
